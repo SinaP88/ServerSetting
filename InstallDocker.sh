1a37a02a8bc191639c82e5b8961b1829d883541f
Sina Parchami <133245535+SinaP88@users.noreply.github.com>
2023-05-26 10:34:27 +0200

    Update InstallDocker.sh
    Docker Engine Installation Added

--- InstallDocker.sh
+++ InstallDocker.sh
@@ -1,1 +1,30 @@
-
+#!/bin/bash
+set -e
+## Install Docker Engine on Ubuntu 20.04 LTS
+sudo apt-get update
+sudo apt install net-tools -y
+sudo apt-get install cmake g++ -y
+sudo apt install libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6 -y
+# Uninstall any such older versions before attempting to install a new version:
+# sudo apt-get remove docker docker-engine docker.io containerd runc
+## Set up the repository
+# Update the apt package index and install packages to allow apt to use a repository over HTTPS:
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg -y
+# Add Docker’s official GPG key:
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+# Use the following command to set up the repository:
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+## Install Docker Engine
+#Update the apt package index:
+sudo apt-get update
+# Install Docker Engine, containerd, and Docker Compose.
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+echo "==========================="
+echo "  Docker Engine Installed  "
+echo "==========================="
